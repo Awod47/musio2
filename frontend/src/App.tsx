197321364dfe043938f2d54d6d75b9593db92a61
Ashutosh <ashutoshwodeyarak47@gmail.com>
2025-03-01 16:46:57 +0100

    frontend boiler plate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { Button } from "./components/ui/button"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign in</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </>
+  )
+}
+
+export default App
